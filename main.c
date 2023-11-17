172e14571daf97656c13a5e33eb9616f606aa80f
carlsalam <carl.salamouny.1@ens.etsmtl.ca>
2023-11-17 17:56:10 -0500

    creation du main

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "t_circuit.h"
+#include "t_entree.h"
+#include "t_file_porte.h"
+#include "t_pin_entree.h"
+#include "t_pin_sortie.h"
+#include "t_porte.h"
+#include "t_sortie.h"
+
+
+
+int main(void){
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
