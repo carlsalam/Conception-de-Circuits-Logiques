be9ebc6fedb1f12a13cbdb5606d5793b46111523
manelehoceini <150092831+manelehoceini@users.noreply.github.com>
2023-11-17 21:58:42 -0500

    Commentaire sur t_entree

--- t_entree.c
+++ t_entree.c
@@ -1,39 +1,39 @@
-//
-// Created by csala on 2023-11-17.
-//
-
-
-
+#define _CRT_SECURE_NO_WARNINGS
 #include "t_entree.h"
-
 
 
 t_entree *t_entree_init(int num)
 {
-    t_entree* nouvel_entree = NULL;
 
-    nouvel_entree = (t_entree*) malloc(sizeof(t_entree));
-    if(nouvel_entree == NULL)
+    //Initialisation du pointeur vers NULL
+    t_entree* nouvelle_entree = NULL;
+
+    //Réservation de mémoire pour la nouvelle entrée et verifier si malloc fonctionne
+    nouvelle_entree = (t_entree*) malloc(sizeof(t_entree));
+    if(nouvelle_entree == NULL)
     {
         return NULL;
     }
 
-    nouvel_entree->id = num;
+    //Initialisation de l'ID
+    nouvelle_entree->id = num;
 
 
-    char entree[12];
-    sprintf(entree, "E%d", num);
+    char entree[10]; //Initialisation d'une chaine de caractère où l'on va stocker le nom.
+    sprintf(entree, "E%d", num); //Formatage
 
-    nouvel_entree->nom = (char *) malloc(strlen(entree) + 1);
-
-    if(nouvel_entree->nom == NULL)
+    //Réservation de mémoire pour le nom et verifier si malloc fonctionne
+    nouvelle_entree->nom = (char *) malloc(strlen(entree) + 1);
+    if(nouvelle_entree->nom == NULL)
     {
-        free(nouvel_entree);
+        free(nouvelle_entree);
         return NULL;
     }
-    strcpy(nouvel_entree->nom, entree);
+    //Copier le nom dans nouvelle entrée
+    strcpy(nouvelle_entree->nom, entree);
 
-    nouvel_entree->pin = NULL;
+    //Initialisation du pin de sortie à NULL
+    nouvelle_entree->pin = NULL;
 
-    return nouvel_entree;
+    return nouvelle_entree;
 }
